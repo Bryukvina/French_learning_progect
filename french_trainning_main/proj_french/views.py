4d8f47aa69dff1baa288c13dd8d6b8453f804f76
Bryukvina Darja <100697083+Bryukvina@users.noreply.github.com>
2026-03-27 23:21:40 -0700

    Загружены файлы первого варианта проекта
    В данной версии не доработаны страницы файлы .html, а также связи между файлами. Верси представляет собой черновой вариант

---
+++ french_trainning_main/proj_french/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.core.cache import cache
+from . import terms_work
+from django.core.paginator import Paginator
+
+
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+
+"""
+def terms_list(request):
+    terms_data = terms_work.get_terms_for_table()
+    # Получаем параметры из GET-запроса
+    page = request.GET.get('page', 1)
+    per_page = request.GET.get('per_page', 10)
+    
+    # Создаем пагинатор
+    paginator = Paginator(terms_data, per_page)
+    
+    try:
+        terms_page = paginator.page(page)
+    except:
+        terms_page = paginator.page(1)
+    
+    context = {
+        'terms_page': terms_page,
+        'paginator': paginator,
+        'per_page': per_page,
+    }
+    return render(request, 'terms_list.html', context)"""
+
+def term_list(request):
+    terms = terms_work.get_terms_for_table()
+    return render(request, "term_list.html", context={"terms": terms})
+
+
+def add_term(request):
+    return render(request, "term_add.html")
+
+def learn_words(request):
+    return render(request, "learn_cards.html")
+
+def send_term(request):
+    if request.method == "POST":
+        cache.clear()
+        user_name = request.POST.get("name")
+        new_term = request.POST.get("new_term", "")
+        new_definition = request.POST.get("new_definition", "").replace(";", ",")
+        context = {"user": user_name}
+        if len(new_definition) == 0:
+            context["success"] = False
+            context["comment"] = "Описание должно быть не пустым"
+        elif len(new_term) == 0:
+            context["success"] = False
+            context["comment"] = "Термин должен быть не пустым"
+        else:
+            context["success"] = True
+            context["comment"] = "Ваш термин принят"
+            terms_work.write_term(new_term, new_definition)
+        if context["success"]:
+            context["success-title"] = ""
+        return render(request, "term_request.html", context)
+    else:
+        add_term(request)
+
+
+def show_stats(request):
+    stats = terms_work.get_terms_stats()
+    return render(request, "stats.html", stats)
+
+
+def check_term(request):
+    return render(request, "check_term.html")
+
